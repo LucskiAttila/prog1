a7d187896739634825af9bd1060bdc080e0f8a7c
vajkone <43681926+vajkone@users.noreply.github.com>
2019-03-19 12:53:15 +0100

    Update ExorTitkosito.java

--- caesar/ExorTitkosito.java
+++ caesar/ExorTitkosito.java
@@ -1,43 +1,43 @@
-public class ExorTitkosito {
-    
-    public ExorTitkosito(String kulcsSzöveg,
-            java.io.InputStream bejövőCsatorna,
-            java.io.OutputStream kimenőCsatorna)
-            throws java.io.IOException {
-        
-        byte [] kulcs = kulcsSzöveg.getBytes();
-        byte [] buffer = new byte[256];
-        int kulcsIndex = 0;
-        int olvasottBájtok = 0;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        while((olvasottBájtok =
-                bejövőCsatorna.read(buffer)) != -1) {
-            
-            for(int i=0; i<olvasottBájtok; ++i) {
-                
-                buffer[i] = (byte)(buffer[i] ^ kulcs[kulcsIndex]);
-                kulcsIndex = (kulcsIndex+1) % kulcs.length;
-                
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+            FileInputStream fin;
+            FileOutputStream fout;
+
+            fin=new FileInputStream("tiszta.txt");
+            int c;
+            ArrayList<Integer> input = new ArrayList<Integer>();
+            ArrayList<Integer> output = new ArrayList<Integer>();
+            Scanner in = new Scanner(System.in);
+            System.out.println("Adja meg a kulcsot: ");
+            String kulcs = in.nextLine();
+            int kulcs_hossz = kulcs.length();
+
+            while((c=fin.read())!=-1)
+            {
+                input.add(c);
             }
-            
-            kimenőCsatorna.write(buffer, 0, olvasottBájtok);
-            
+            fout = new FileOutputStream("titkos.txt");
+            for(int i=0;i<input.size();i++)
+            {
+                output.add(input.get(i)^key.charAt(i%kulcs_hossz));
+                fout.write(output.get(i));
+            }
+
+            for(int i=0;i<output.size();i++)
+            {
+                System.out.printf("%c",output.get(i));
+            }
+
+            fin.close();
+            fout.close();
+
         }
-        
-    }
-    
-    public static void main(String[] args) {
-        
-        try {
-            
-            new ExorTitkosito(args[0], System.in, System.out);
-            
-        } catch(java.io.IOException e) {
-            
-            e.printStackTrace();
-            
-        }
-        
-    }
-    
 }
